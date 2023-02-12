e851c4e8872822ea3c10ab12bf27a0d1f801ff77
Vincent PERON <peron.vincent23@gmail.com>
2023-02-12 15:58:23 +0100

    build: add recipeController with TODO functions

---
+++ app/controllers/recipeController.js
@@ -0,0 +1,23 @@
+const { Category, Level, Recipe } = require('../models');
+
+const recipeController = {
+
+
+// recipesList
+
+// category
+
+// level
+
+// showRecipe
+
+// submitRecipe
+
+// addRecipeInDb
+
+// searchRecipe
+
+
+};
+
+module.exports = recipeController;
